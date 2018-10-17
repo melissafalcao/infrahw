666fab26ad7fae269d1c48bc287be28e89df5ea8
Éden Santana <eeas@cin.ufpe.br>
2018-10-17 20:06:17 -0300

    Update and rename MUX4.v to mux4.v

---
+++ mux4.v
@@ -0,0 +1,24 @@
+module mux4(
+    input pc[31:0],
+    input mdr[31:0],
+    input a[31:0],
+    input ULAa[1:0],
+    output Mux4out[31:0]
+);
+
+begin  
+    case (ULAa)
+        2'b00: begin
+            Mux4out[31:0] <= pc[31:0]
+        end
+        2'b01: begin
+            Mux4out[31:0] <= mdr[31:0]
+        end
+        2'b10: begin
+            Mux4out[31:0] <= a[31:0]
+        end
+    endcase
+    
+end
+    
+endmodule: mux4
