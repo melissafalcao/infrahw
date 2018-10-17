9c47944f13cc098546ce03912a9d4e1303a6e310
Lúcio Melo <lfmm2@cin.ufpe.br>
2018-10-17 19:48:12 -0300

    Update MUX6.v

--- MUX6.v
+++ MUX6.v
@@ -1,7 +1,27 @@
-module MUX6(PCmux, a, ULAout, SLAC, EPCout, MDRout, ULAresult, MUX6out);
+module mux6(
+   input logic [2:0] PCmux,
+   input logic [31:0] A,
+   input logic [31:0] ULAout,
+   input logic [31:0] SLAC,
+   input logic [31:0] EPCout,
+   input logic [31:0] MDRout,
+   input logic [31:0] ulaResult,
+   output logic [31:0] MUX6out);
 
-	input [2:0]  PCmux; 
-	input [31:0]  a, ULAout, SLAC, EPCout, MDRout, ULAresult; 
-	output [31:0]  MUX6out;
+always_comb begin
+   case (PCmux)
+      3'b000
+         MUXout[31:0] <= A[31:0];
+      3'b001
+         MUXout[31:0] <= ULAout[31:0];
+      3'b010
+         MUXout[31:0] <= SLAC[31:0];
+      3'b011
+         MUXout[31:0] <= EPCout[31:0];
+      3'b100
+         MUXout[31:0] <= MDRout[31:0];
+      3'b101
+         MUXout[31:0] <= ulaResult[31:0];
+end
 
-endmodule
+endmodule: mux6
