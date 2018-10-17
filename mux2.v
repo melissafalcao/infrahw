a4156a6af0f5689bde089bf46270decbff1588d4
Éden Santana <eeas@cin.ufpe.br>
2018-10-17 20:37:43 -0300

    Update mux2.v

--- mux2.v
+++ mux2.v
@@ -1,33 +1,29 @@
-module mux2(
-    input [2:0] WriteData,
-    input [31:0] ulaOut,
-    input [31:0] LSout,
-    input [31:0] HIout,
-    input [31:0] LOout,
-    input [31:0] Shiftout,
-    input [31:0] LT32,
-    output [31:0] MUX2out);
+module MUX2 (WriteData, ULAout, LSout, HIout, LOout, Shiftout, LT32, MUX2out)
+	
+input [2:0] WriteData;
+input [31:0]  ULAout, LSout, HIout, LOout, Shiftout, LT32;
+output [31:0] MUX2out;
     
 begin
     case(WriteData)
         3'b000: begin
-		Mux2out[31:0] <= ulaOut[31:0];
+		MUX2out[31:0] <= ULAout[31:0];
 	end
 	3'b001: begin
-		Mux2out[31:0] <= LSout[31:0];
+		MUX2out[31:0] <= LSout[31:0];
 	end
 	3'b010: begin
-		Mux2out[31:0] <= HIout[31:0];
+		MUX2out[31:0] <= HIout[31:0];
 	end
 	3'b011: begin
-		Mux2out[31:0] <= LOout[31:0];
+		Mux2MUX2outout[31:0] <= LOout[31:0];
         end
         3'b100: begin
-		Mux2out[31:0] <= Shiftout[31:0];
+		MUX2out[31:0] <= Shiftout[31:0];
         end
         3'b101: begin
-		Mus2out[31:0] <= LT32[31:0];
+		MUX2out[31:0] <= LT32[31:0];
         end
 end
 
-endmodule: mux2
+endmodule: MUX2
