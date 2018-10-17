c9415e9caa417947c2cdee8232e14cd309644813
Éden Santana <eeas@cin.ufpe.br>
2018-10-17 19:56:02 -0300

    Tirando coisas de system verilog aqui tambem

--- mux2.v
+++ mux2.v
@@ -1,32 +1,32 @@
 module mux2(
-    input logic[31:0] ulaOut,
-    input logic[31:0] LSout,
-    input logic[31:0] HIout,
-    input logic[31:0] LOout,
-    input logic[31:0] Shiftout,
-    input logic[31:0] LT32,
-    input logic[2:0] WriteData,
-    output logic[31:0] MUX2out);
+    input [31:0] ulaOut,
+    input [31:0] LSout,
+    input [31:0] HIout,
+    input [31:0] LOout,
+    input [31:0] Shiftout,
+    input [31:0] LT32,
+    input [2:0] WriteData,
+    output [31:0] MUX2out);
     
-always_comb begin
+begin
     case(WriteData)
         3'b000: begin
-			Mux2out[31:0] <= ulaOut[31:0];
-		end
-		3'b001: begin
-			Mux2out[31:0] <= LSout[31:0];
-		end
-		3'b010: begin
-			Mux2out[31:0] <= HIout[31:0];
-		end
-		3'b011: begin
-			Mux2out[31:0] <= LOout[31:0];
+		Mux2out[31:0] <= ulaOut[31:0];
+	end
+	3'b001: begin
+		Mux2out[31:0] <= LSout[31:0];
+	end
+	3'b010: begin
+		Mux2out[31:0] <= HIout[31:0];
+	end
+	3'b011: begin
+		Mux2out[31:0] <= LOout[31:0];
         end
         3'b100: begin
-			Mux2out[31:0] <= Shiftout[31:0];
+		Mux2out[31:0] <= Shiftout[31:0];
         end
         3'b101: begin
-			Mus2out[31:0] <= LT32[31:0];
+		Mus2out[31:0] <= LT32[31:0];
         end
 end
 
