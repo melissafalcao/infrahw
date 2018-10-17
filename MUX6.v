3d8966b40aaa51a582e5dadb1564b24834ab6374
Éden Santana <eeas@cin.ufpe.br>
2018-10-17 20:44:12 -0300

    Update and rename mux6.v to MUX6.v

--- MUX6.v
+++ MUX6.v
@@ -1,14 +1,8 @@
-module mux6(
-   input [2:0] PCmux,
-   input [31:0] A,
-   input [31:0] ULAout,
-   input [31:0] SLAC,
-   input [31:0] EPCout,
-   input [31:0] MDRout,
-   input [31:0] ulaResult,
-   input [31:0] Mem,
-   output [31:0] MUX6out);
-
+module MUX6(PCmux, A, ULAout, SLAC, EPCout, MDRout, ULAresult, MUX6out);
+ 	input [2:0]  PCmux; 
+	input [31:0]  a, ULAout, SLAC, EPCout, MDRout, ULAresult; 
+	output [31:0]  MUX6out;
+   
 begin
    case (PCmux)
       3'b000
@@ -22,9 +16,9 @@ begin
       3'b100
          MUX6out[31:0] <= MDRout[31:0];
       3'b101
-         MUX6out[31:0] <= ulaResult[31:0];
+      MUX6out[31:0] <= ULAResult[31:0];
       3'b110
          MUX6out[31:0] <= Mem[31:0];
 end
 
-endmodule: mux6
+endmodule: MUX6
