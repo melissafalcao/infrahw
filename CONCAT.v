838102bd7b3232c5404a8ee88abcbe8ff09fab43
LucasMendoncacc <32796404+LucasMendoncacc@users.noreply.github.com>
2018-10-17 20:14:29 -0300

    Update CONCAT.v

--- CONCAT.v
+++ CONCAT.v
@@ -1,9 +1,9 @@
-module CONCAT (a, b, c, concatout);
+module CONCAT (rs, rt, immediato, concatout);
 
 //concaternar a, b, c
-input [4:0] a;
-input [4:0] b;
-input [15:0] c;
+  input [4:0] rs;
+  input [4:0] rt;
+  input [15:0] immediato;
 output [25:0] concatout;
 
 endmodule
