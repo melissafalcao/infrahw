bde6f9efdae3a8592909c687d0a5d1fa839fef4c
Éden Santana <eeas@cin.ufpe.br>
2018-10-17 19:42:47 -0300

    Update mux1.v
    Corrigindo nome da saida para Mux1out

--- mux1.v
+++ mux1.v
@@ -13,28 +13,28 @@ module mux1(
 always_comb begin
 	case (MemoryAdress)
 		3'b000: begin
-			out[31:0] <= pc[31:0];
+			Mux1out[31:0] <= pc[31:0];
 		end
 		3'b001: begin
-			out[31:0] <= ulaResult[31:0];
+			Mux1out[31:0] <= ulaResult[31:0];
 		end
 		3'b010: begin
-			out[31:0] <= ext16_32[31:0];
+			Mux1out[31:0] <= ext16_32[31:0];
 		end
 		3'b011: begin
-			out[31:0] <= ulaOut[31:0];
-        end
-        3'b100: begin
-			out[31:0] <= 32'd253;
-        end
-        3'b101: begin
-			out[31:0] <= 32'd254;
-        end
-        3'b110: begin
-			out[31:0] <= 32'd255;
-        end
-        3'b111: begin
-			out[31:0] <= ext25_32[31:0];
+			Mux1out[31:0] <= ulaOut[31:0];
+        	end
+        	3'b100: begin
+			Mux1out[31:0] <= 32'd253;
+        	end
+        	3'b101: begin
+			Mux1out[31:0] <= 32'd254;
+        	end
+        	3'b110: begin
+			Mux1out[31:0] <= 32'd255;
+        	end
+        	3'b111: begin
+			Mux1out[31:0] <= ext25_32[31:0];
 		end
 	endcase
 end
