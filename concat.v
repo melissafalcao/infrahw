218822dadbcbfdb1aff2d7680ff4b1f95944ce03
Jose Gabriel Silva Pereira <jgsp2@windows.cin.ufpe.br>
2018-10-17 13:05:28 -0300

    problemas com renomeamento de arquivos sendo resolvidos

--- concat.v
+++
@@ -1,9 +0,0 @@
-module CONCAT (a, b, c, concatout);
-
-//concaternar a, b, c
-input [4:0] a;
-input [4:0] b;
-input [15:0] c;
-output [25:0] concatout;
-
-endmodule
--- pc.v
+++
@@ -1,7 +0,0 @@
-module pc(PCwrite, mux6OUT, pc); 
-
-input PCwrite;
-input [31:0] mux6OUT;
-output [31:0] pc;
-
-endmodule
