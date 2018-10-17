989e9e5bac074b03eb4ce95316c831248708fb8c
Lúcio Melo <lfmm2@cin.ufpe.br>
2018-10-17 20:03:33 -0300

    Update MUX5.v

--- MUX5.v
+++ MUX5.v
@@ -1,7 +1,24 @@
-module MUX5(ULAb, B, ext16_32, reg4, reg1, ext16_32_left_shifted, MUX5out);
+module mux5(
+   input [2:0] ULAb,
+   input [31:0]  B,
+   input [31:0] ext16_32,
+   input [31:0] reg4,
+   input [31:0] reg1,
+   input [31:0] ext16_32_left_shifted,
+   output [31:0] MUX5out);
+ 
+begin
+   case (PCmux)
+      3'b000
+         MUX5out[31:0] <= B[31:0];
+      3'b001
+         MUX5out[31:0] <= ext16_32[31:0];
+      3'b010
+         MUX5out[31:0] <= reg4[31:0];
+      3'b011
+         MUX5out[31:0] <= reg1[31:0];
+      3'b100
+         MUX5out[31:0] <= ext16_32_left_shifted[31:0];
+end
 
-	input [2:0] ULAb; 
-	input [31:0]  B, ext16_32, reg4, reg1, ext16_32_left_shifted; 
-	output [31:0] MUX5out;
-
-endmodule
+endmodule: mux5
