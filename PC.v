9038a2ba223ce94354f388675f9b962f2249e44d
zegabr <jgsp2@cin.ufpe.br>
2018-10-18 00:52:59 -0300

    pc alterado na tentativa de compilar

--- PC.v
+++ PC.v
@@ -2,12 +2,15 @@ module pc(PCwrite, mux6OUT, pc);
 
 input PCwrite;
 input [31:0] mux6OUT;
-output [31:0] pc;
-
+output reg [31:0] pc;
+/*
 case (PCwrite)
   1'd1:pc = mux6OUT;
   1'd0:pc; 
   default:; 
-endcase
+endcase*/
+if(PCwrite==1)begin
+  pc=mux6OUT;
+end
 
 endmodule
