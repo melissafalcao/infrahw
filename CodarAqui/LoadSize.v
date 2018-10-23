aec976c9de7d4e32884a34f893425c423d9167b6
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2018-10-23 19:25:35 -0300

    StoreSize e LoadSize funcionando

--- CodarAqui/LoadSize.v
+++ CodarAqui/LoadSize.v
@@ -4,4 +4,20 @@ input [1:0] LS;
 input [31:0] MDRout;
 output [31:0] LSout;
 
+always @(*)
+begin
+	case (LS)
+		2'b00: begin//lb
+			LSout[31:0] <=  { 26'd0, MDRout[7:0] };
+		end
+		2'b01: begin//lh
+			LSout[31:0] <=  { 16'd0, MDRout[15:0] };
+		end
+		2'b10: begin//lw
+			LSout[31:0] <=  MDRout[31:0];
+		end
+	endcase
+end
+
+
 endmodule
