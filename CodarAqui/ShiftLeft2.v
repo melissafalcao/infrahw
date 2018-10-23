9134b85f4a72fd229faaa90996cf4dc3230abd33
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2018-10-23 15:42:47 -0300

    ShiftLeft2 funcionando

--- CodarAqui/ShiftLeft2.v
+++ CodarAqui/ShiftLeft2.v
@@ -1,6 +1,11 @@
-module ShiftLeft2(ext16_32, ext16_32_left_shift);
+module ShiftLeft2(ext16_32, ext16_32_left_shifted);
 
 	input [31:0]  ext16_32; 
-	output [31:0]  ext16_32_left_shift; 
+	output [31:0]  ext16_32_left_shifted; 
+
+always @(*)
+begin
+      ext16_32_left_shifted[31:0] <= ext16_32[31:0] << 2'd2;
+end
 
 endmodule
