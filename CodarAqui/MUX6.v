44205f7cdfab19901e456c864779d36e52fe0e11
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2018-10-23 16:34:59 -0300

    mux6 funcionando

--- CodarAqui/MUX6.v
+++ CodarAqui/MUX6.v
@@ -6,21 +6,28 @@ module MUX6(PCmux, A, ULAout, SLAC, EPCout, MDRout, ULAresult, Mem, MUX6out);
 always @(*)
 begin
    case (PCmux)
-      3'b000:
-         MUX6out[31:0] <= A[31:0];
-      3'b001:
-         MUX6out[31:0] <= ULAout[31:0];
-      3'b010:
-         MUX6out[31:0] <= SLAC[31:0];
-      3'b011:
-         MUX6out[31:0] <= EPCout[31:0];
-      3'b100:
-         MUX6out[31:0] <= MDRout[31:0];
-      3'b101:
-      MUX6out[31:0] <= ULAresult[31:0];
-      3'b110:
-         MUX6out[31:0] <= Mem[31:0];
-         endcase
+      3'b000:begin
+        MUX6out[31:0] <= A[31:0];
+      end
+      3'b001:begin
+        MUX6out[31:0] <= ULAout[31:0];
+      end
+      3'b010:begin
+        MUX6out[31:0] <= SLAC[31:0];
+      end
+      3'b011:begin
+        MUX6out[31:0] <= EPCout[31:0];
+      end
+      3'b100:begin
+        MUX6out[31:0] <= MDRout[31:0];
+      end
+      3'b101:begin
+        MUX6out[31:0] <= ULAresult[31:0];
+      end
+      3'b110:begin
+        MUX6out[31:0] <= Mem[31:0];
+      end
+    endcase
 end
 
 endmodule
