018b97ab69697a4ba8f6e2f5c301c1d2d80a5d23
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2018-10-23 16:00:10 -0300

    ShiftLeft2Concat funcionando

--- CodarAqui/ShiftLeft2Concat.v
+++ CodarAqui/ShiftLeft2Concat.v
@@ -1,7 +1,12 @@
-module SighLeft2Concat(concatout, pc, SLAC);
+module ShiftLeft2Concat(concatout, PC, SLAC);
 
 	input [25:0] concatout; 
-	input [31:0]  pc; 
-	output [31:0]  SLAC;
+	input [31:0]  PC; 
+	output reg [31:0]  SLAC;
+
+always @(*)
+begin
+	SLAC[31:0] <= {PC[31:28], concatout[25:0], 2'd0};//concatena PC com concatout(offset) extendido de 2 bits para a esquerda  
+end
 
 endmodule
