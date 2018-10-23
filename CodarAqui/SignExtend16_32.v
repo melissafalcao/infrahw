cb99b863c36a77913b3f83a671e3805a6de1e3d5
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2018-10-23 15:30:20 -0300

    SignExtend funcionando

--- CodarAqui/SignExtend16_32.v
+++ CodarAqui/SignExtend16_32.v
@@ -1,10 +1,11 @@
-module SignExtend16_32(
-	input [15:0]  imediato, 
-	output reg [31:0]  ext16_32);
+//FUNCIONANDO
+module SignExtend16_32(imediato,  ext16_32);
+	input [15:0]  imediato,;
+	output reg [31:0]  ext16_32;
 	
 always @(*)
 begin
-	ext16_32[15:0] <= imediato[15:0] << 16'd16;
+	ext16_32[15:0] <= {16'd0, imediato[15:0]};
 end 
 
 endmodule
