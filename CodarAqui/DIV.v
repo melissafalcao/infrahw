2e4d68b0cb3d59c3ba5677a5efb15fecac461cf4
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2018-10-23 20:13:20 -0300

    div nao funciona ainda por razoes desconhecidas

--- CodarAqui/DIV.v
+++ CodarAqui/DIV.v
@@ -1,14 +1,19 @@
+//NAO FUNCIONA AINDA	
 module DIV(clock, reset, A, B, hidiv, lodiv, Div0);
 
 	input wire clock, reset;
 	input [31:0] A, B;
-	output [31:0] hidiv, lodiv; 
+	output reg[31:0] hidiv, lodiv; 
 	output reg Div0;
-	
-initial begin
 	reg [31:0] Quociente;
-	reg [63:0] Resto <= {32'd0, A[31:0]};
-	reg [63:0] Divisor <= {32'd0, B[31:0]};
+	reg [63:0] Resto ;
+	reg [63:0] Divisor ;
+	reg [5:0] i;
+initial begin
+	  Resto[63:0] = {32'd0, A[31:0]};
+	  Divisor[63:0] = { B[31:0],32'd0};
+	  Quociente[31:0] = 32'd0;
+	  Div0 = 1'b0;
 end
 
 always @(*)
@@ -18,21 +23,21 @@ begin
 	end 
 	
 	if(B[31:0] != 32'd0) begin		//
-		for (i = 0; i =< 32; i++) begin
+		for (i =6'd0; i <= 6'd32; i=i+1) begin
 			Resto[63:0] <= Resto[63:0] - Divisor[63:0];
-			if(R[63:0] >= 63'd0) begin
+			if(Resto[63:0] >= 64'd0) begin
 				Quociente[31:0] <= Quociente[31:0] << 1'd1;
 				Quociente[0] <= 1;
 				Divisor[63:0] <= Divisor[63:0] >> 1'd1;
-			end
-			if(Resto[63:0] < 63'd0) begin
+			end//endif
+			if(Resto[63:0] < 64'd0) begin
 				Resto[63:0] <= Resto[63:0] + Divisor[63:0];
 				Quociente[31:0] <= Quociente[31:0] << 1'd1;
 				Quociente[0] <= 0;
 				Divisor[63:0] <= Divisor[63:0] >> 1'd1;
-			end
-		end
-	end
+			end//endif
+		end//endfor
+	end//endif
 	hidiv[31:0] <= Resto[31:0];
 	lodiv[31:0] <= Quociente[31:0];
 end
