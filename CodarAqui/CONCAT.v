5ef226b3edc7547730c0a9df77903590fa72caef
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2018-10-23 15:19:43 -0300

    CONCAT funcionando

--- CodarAqui/CONCAT.v
+++ CodarAqui/CONCAT.v
@@ -1,9 +1,16 @@
+//FUNCIONANDO
 module CONCAT (rs, rt, imediato, concatout);
 
-//concaternar rs,rt e imediato (conferir a ordem da concatenacao, zé n tem certeza)
+//concaternar rs,rt e imediato
   input [4:0] rs;
   input [4:0] rt;
-  input [15:0] immediato;
-output [25:0] concatout;
+  input [15:0] imediato;
+  output reg [25:0] concatout;
+
+always @(*)
+begin
+  concatout[25:0] <= {rs[4:0], rt[4:0], imediato[15:0]};
+end
+
 
 endmodule
