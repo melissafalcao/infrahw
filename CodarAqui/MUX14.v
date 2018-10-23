021d3f292931a48a5f0fe17aad9b34b995e17235
Lúcio Melo <lfmm2@cin.ufpe.br>
2018-10-23 15:55:19 -0300

    Update MUX14.v

--- CodarAqui/MUX14.v
+++ CodarAqui/MUX14.v
@@ -1,7 +1,18 @@
-module MUX14(mux14, ULAresult, LSout, mux14out);
+module MUX14(MUX14, ulaResult, LSout, MUX14out);
 
-	input mux14; 
-	input [31:0]  ULAresult, LSout; 
-	output [31:0]  mux14out;
+input [0:1] MUX14; 
+input [31:0]  ulaResult, LSout; 
+output [31:0]  MUX14out;
 
+always @(*)
+begin
+    case (MUX14)
+        1'b0: begin
+            MUX14out[31:0] <= ulaResult[31:0];
+        end
+        1'b1: begin
+        	MUX14out[31:0] <= LSout[31:0];
+        end
+    endcase
+end
 endmodule
