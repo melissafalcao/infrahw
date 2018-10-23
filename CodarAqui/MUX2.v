9bd4c481cfa662ad99ec9b873ccc83e718700d77
Jose Gabriel Silva Pereira <jgsp2@WINDOWS.CIN.UFPE.BR>
2018-10-23 14:24:20 -0300

    mux2 funcionando

--- CodarAqui/MUX2.v
+++ CodarAqui/MUX2.v
@@ -1,3 +1,4 @@
+//FUNCIONANDO
 module MUX2 (WriteData, ULAout, LSout, HIout, LOout, Shiftout, LT32, MUX2out);
 	
 input [2:0] WriteData;
@@ -7,25 +8,25 @@ output reg[31:0] MUX2out;
 always @(*)
 begin
     case(WriteData)
-        3'b000: begin
-		MUX2out <= ULAout;
-	end
-	3'b001: begin
-		MUX2out <= LSout;
-	end
-	3'b010: begin
-		MUX2out <= HIout;
-	end
-	3'b011: begin
-		MUX2out<= LOout;
-        end
-        3'b100: begin
-		MUX2out <= Shiftout;
-        end
-        3'b101: begin
-		MUX2out <= LT32;
-        end
-       endcase
+		3'b000: begin
+			MUX2out[31:0] <= ULAout[31:0];
+		end
+		3'b001: begin
+			MUX2out[31:0] <= LSout[31:0];
+		end
+		3'b010: begin
+			MUX2out[31:0] <= HIout[31:0];
+		end
+		3'b011: begin
+			MUX2out[31:0]<= LOout[31:0];
+		end
+		3'b100: begin
+			MUX2out[31:0] <= Shiftout[31:0];
+		end
+		3'b101: begin
+			MUX2out[31:0] <= LT32[31:0];
+		end
+    endcase
 end
 
 endmodule
