e050c2400cb78a32c805a165e21b96678df9004b
Lúcio Melo <lfmm2@cin.ufpe.br>
2018-10-23 16:02:00 -0300

    Update UnitExtend.v

--- CodarAqui/UnitExtend.v
+++ CodarAqui/UnitExtend.v
@@ -1,6 +1,10 @@
 module UnitExtend(LT, LT32);
 
-	input LT; 
-	output [31:0]  LT32; 
-
+	input [1:0] LT; 
+	output [31:0] LT32; 
+//Estende LT com 1 bit para 32 bits
+always @(*)
+begin  
+    LT32 [31:0] <= {31'd0, LT[1:0]};
+end
 endmodule
