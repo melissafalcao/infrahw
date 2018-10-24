a1f584514914273763702f2ffe26255e9370cf57
LucasMendoncacc <32796404+LucasMendoncacc@users.noreply.github.com>
2018-10-24 16:57:56 -0300

    Update MULT.v
    agoravai dobro

--- CodarAqui/MULT.v
+++ CodarAqui/MULT.v
@@ -1,11 +1,10 @@
-module MULT(clock, reset, comeco, a, b, hi, lo,i);
+module MULT(clock, reset, comeco, a, b, hi, lo);
 
     input wire reset;
     input wire clock;
     input wire comeco;
-	input [31:0] a, b;  //fazer div e multi aqui jogando a saida no hi e lo
+	input [31:0] a, b;  //fazer multi aqui jogando a saida no hi e lo
 	output reg [31:0] hi, lo; 
-	output reg i;
 
 
     reg [64:0] multiplicando;
@@ -16,7 +15,7 @@ module MULT(clock, reset, comeco, a, b, hi, lo,i);
     parameter espera = 2'd0;
     parameter inicial = 2'd1;
     parameter repeticao = 2'd2;
-    parameter final = 2'd3;
+    parameter fim = 2'd3;
 
     initial begin
         multiplicando = {65'd0};
@@ -30,10 +29,10 @@ module MULT(clock, reset, comeco, a, b, hi, lo,i);
             
 
   always @(posedge clock) begin
-        if(reset == 1'b1) begin
+        if(reset == 1'd1) begin
             multiplicando = {65'd0};
             multiplicador = {65'd0};
-            contador = {4'd0};
+            contador = {5'd0};
             estado = espera;
             hi = {32'd0};
             lo = {32'd0};
@@ -51,31 +50,57 @@ module MULT(clock, reset, comeco, a, b, hi, lo,i);
         end
         repeticao: begin
             if(multiplicador[1]==0 && multiplicador[0]==0)begin
-            multiplicador = (multiplicador>>>1);
+				if(multiplicador[64]==1'd1) begin
+					multiplicador = multiplicador >>> 1;
+					multiplicador[64]=1;
+				end
+				else begin
+					multiplicador = multiplicador >>> 1;
+				end
             contador = contador + 1'd1;
             end
             else if(multiplicador[1]==1 && multiplicador[0]==1)begin
-            multiplicador = (multiplicador>>>1);
+				if(multiplicador[64]==1'd1) begin
+					multiplicador = multiplicador >>> 1;
+					multiplicador[64]=1;
+				end
+				else begin
+					multiplicador = multiplicador >>> 1;
+				end
             contador = contador + 1'd1;
             end
             else if(multiplicador[1]==0 && multiplicador[0]==1)begin
             multiplicador = multiplicador + multiplicando;
-            multiplicador = (multiplicador>>>1);
+				if(multiplicador[64]==1'd1) begin
+					multiplicador = multiplicador >>> 1;
+					multiplicador[64]=1;
+				end
+				else begin
+					multiplicador = multiplicador >>> 1;
+				end
             contador = contador + 1'd1;
             end
             else if(multiplicador[1]==1 && multiplicador[0]==0) begin
             multiplicador = multiplicador - multiplicando;
-            multiplicador = (multiplicador>>>1);
+				if(multiplicador[64]==1'd1) begin
+					multiplicador = multiplicador >>> 1;
+					multiplicador[64]=1;
+				end
+				else begin
+					multiplicador = multiplicador >>> 1;
+				end
             contador = contador + 1'd1;
             end
-            if (contador == 5'd32) begin
-              estado = final;
-            end
+        if (contador == 5'd32) begin
+            estado = fim;
         end
-        final: begin
+        end
+        fim: begin
           hi = multiplicador[64:33];
           lo = multiplicador[32:1];
           estado = espera;
         end
-
+        endcase
+    end
 endmodule
+
