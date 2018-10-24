d8bfc93a444d5e17314417a9b2f49fa55a4c1436
Lúcio Melo <lfmm2@cin.ufpe.br>
2018-10-24 17:18:06 -0300

    Update DIV.v

--- CodarAqui/DIV.v
+++ CodarAqui/DIV.v
@@ -1,15 +1,15 @@
 //NAO FUNCIONA AINDA	
-module DIV(clock, reset, operacao, A, B, hidiv, lodiv, Div0);
+module testdiv(clock, reset, A, B, hidiv, lodiv, Div0);
 
-	input wire clock, reset, operacao;
+	input wire clock, reset;
 	input[31:0] A, B;
 	output reg[31:0] hidiv, lodiv;
-	output wire Div0;
+	output reg Div0;
 	
-	reg[31:0] quociente;
-	reg[63:0] divisor;
-	reg[63:0] resto;
-	reg[5:0] contador;
+	reg [31:0] quociente;
+	reg [63:0] divisor;
+	reg [63:0] resto;
+	reg [5:0] contador;
 	reg estado;
 
 	parameter espera = 1'd0;
@@ -19,7 +19,7 @@ module DIV(clock, reset, operacao, A, B, hidiv, lodiv, Div0);
 		resto[63:0] = { 32'd0, A[31:0] };		// inicialmente o resto é o dividendo
 		divisor[63:0] = { B[31:0], 32'd0 };
 		quociente[31:0] = 32'd0;
-		contador[5:0] = 5'd0;
+		contador[5:0] = 6'd0;
 		estado = espera;
 		hidiv[31:0] = 32'd0;
 		lodiv[31:0] = 32'd0;
@@ -28,7 +28,7 @@ module DIV(clock, reset, operacao, A, B, hidiv, lodiv, Div0);
 
 	always @(posedge clock)	begin
 		// só opera se o controle solicitar a divisão:
-		if((operacao == 1'd0)&&(contador != 6'd32)) begin
+		if(contador != 6'd32) begin
 			// se o divisor for zero, seta a excessão:
 			if(B[31:0] == 32'd0) begin
 				Div0 = 1'b1;
@@ -42,24 +42,24 @@ module DIV(clock, reset, operacao, A, B, hidiv, lodiv, Div0);
 				// verifica se ainda está em contagem:
 				if(estado == contagem) begin
 					// algoritmo de subtrações sucessivas:
-					Resto[63:0] = Resto[63:0] - Divisor[63:0];
-					if(Resto[63:0] >= 64'd0) begin
-						Quociente[31:0] = Quociente[31:0] << 1'd1;
-						Quociente[0] = 1;
-						Divisor[63:0] = Divisor[63:0] >> 1'd1;
+					resto[63:0] = resto[63:0] - divisor[63:0];
+					if(resto[63:0] >= 64'd0) begin
+						quociente[31:0] = quociente[31:0] << 1'd1;
+						quociente[0] = 1;
+						divisor[63:0] = divisor[63:0] >> 1'd1;
 					end
-					if(Resto[63:0] < 64'd0) begin
-						Resto[63:0] = Resto[63:0] + Divisor[63:0];
-						Quociente[31:0] = Quociente[31:0] << 1'd1;
-						Quociente[0] = 0;
-						Divisor[63:0] = Divisor[63:0] >> 1'd1;
+					if(resto[63:0] < 64'd0) begin
+						resto[63:0] = resto[63:0] + divisor[63:0];
+						quociente[31:0] = quociente[31:0] << 1'd1;
+						quociente[0] = 0;
+						divisor[63:0] = divisor[63:0] >> 1'd1;
 					end
 					// incrementa a contagem:
 					contador = contador + 6'd1;
 					// se chegar ao fim da contagem, seta as saídas:
 					if(contador == 6'd32) begin
-						hidiv[31:0] <= Resto[31:0];
-						lodiv[31:0] <= Quociente[31:0];
+						hidiv[31:0] = resto[31:0];
+						lodiv[31:0] = quociente[31:0];
 						estado = espera;
 					end
 
