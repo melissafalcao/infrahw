f619f5183bd30b8377d198432e16a2b30da5ab72
LucasMendoncacc <32796404+LucasMendoncacc@users.noreply.github.com>
2018-10-24 12:09:49 -0300

    Update Multi.v

--- CodarAqui/Multi.v
+++ CodarAqui/Multi.v
@@ -1,21 +1,22 @@
-module Multi(a, b, hi, lo,i);
+module Multi(a, b, multiplicando, multiplicador, hi, lo,i);
 
 	input [31:0] a, b;  //fazer div e multi aqui jogando a saida no hi e lo
-	output [31:0] hi, lo; 
-  output reg i;
+	output reg [31:0] hi, lo; 
+	output reg i;
 
 
-    interger multiplicando[64:0];
-    interger multiplicador[64:0];
+    output reg [64:0] multiplicando;
+	output reg [64:0] multiplicador;
 
     initial begin
-        multiplicando[64:0] = {a[31:0],32'd0,1'd0};
-        multiplicador[64:0] = {32'd0,b[31:0],1'd0};
+        multiplicando[64:0] <= {a[31:0],32'd0,1'd0};
+        multiplicador[64:0] <= {32'd0,b[31:0],1'd0};
         
     end
+   
+  always @(*) begin
     
-    
-      for (i = 0; i<32 ; i+1) begin
+      for (i = 0; i<32 ; i=i+1) begin
         if(multiplicador[1]==0 && multiplicador[0]==0)begin
           multiplicador = (multiplicador>>>1);
         end
@@ -33,7 +34,7 @@ module Multi(a, b, hi, lo,i);
       end
       hi = multiplicador[64:33];
       lo = multiplicador[32:1];
-    
+end
 
 
 endmodule
